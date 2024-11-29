47902d88140eac3d07d8442e495879e248ff2504
xuandonghcm <hoangxuandong24@gmail.com>
2024-11-29 11:43:31 +0700

    create project

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Drawer from '@mui/material/Drawer';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import MediaCard from '@/components/MediaCard';
+
+export default function HomePage() {
+  return (
+    <>ahihi</>
+  );
+}
